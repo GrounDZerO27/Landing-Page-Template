f70aa9a75f0767d266aef50c443f69fa8c6df4b2
ZGROUND <rheyandizon@gmail.com>
2023-03-31 02:16:26 +0800

    initial commit

---
+++ src/js/slider.js
@@ -0,0 +1,12 @@
+
+$( document ).ready(function($) {
+    $('.slider').slick({
+        infinite: true,
+          dots: true,
+          arrows: false,
+          autoplay: true,
+          autoplaySpeed: 3000,
+          fade: false,
+          fadeSpeed: 1000
+      });
+});
