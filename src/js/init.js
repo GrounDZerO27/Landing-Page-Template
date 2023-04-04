8692a1e4d3c6433e9a29fc5188908cfab823b4cf
ZGROUND <rheyandizon@gmail.com>
2023-04-04 14:25:23 +0800

    Table Layout

--- src/js/init.js
+++ src/js/init.js
@@ -1,7 +1,50 @@
-(function($){
-  $(function(){
+$(document).ready(function(){
+  console.log('init..');
 
-    $('.sidenav').sidenav();
+  $('.tabs').tabs();
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+  $('.dropdown-trigger').dropdown();
+  
+
+  (function($){
+    $(function(){
+  
+      $('.sidenav').sidenav();
+  
+    }); // end of document ready
+  })(jQuery); // end of jQuery name space
+
+  var hoverItem = $('.img-hover-item'),
+      hoverBtn = $('.img-hover');
+
+  $(hoverItem).hide();
+
+  $(hoverBtn).click(function() {
+    $(hoverItem).show();
+  });
+  
+  $(hoverBtn).mouseover(function(){
+    $(hoverItem).show();
+  });
+  $(".img-hover").mouseout(function(){
+      $(hoverItem).hide();
+  });
+
+ 
+
+});
+
+
+$(".dropdown-trigger").click(function() {
+  console.log('clicked');
+
+  setTimeout(function() {
+    $('.dropdown-content').css({
+      'top': '80px',
+      'left': '-20px'
+    });
+
+    console.log('style init');
+  }, 10);
+
+});
